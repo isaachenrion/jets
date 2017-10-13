b09cee95ce1fcc00de8ec0f141fb3abb64617b12
isaachenrion <isaachenrion@gmail.com>
2017-10-13 17:02:48 -0400

    fix bash

--- four_runs.sh
+++ four_runs.sh
@@ -1,12 +1,8 @@
 
 while [ "$1" != "" ]; do
     case $1 in
-        -i | --iterations )     shift
-                                iterations=$1
-                                ;;
-        -m | --model_type )     model_type=$1
-                                ;;
-        -s | --seed )           seed=$1
+        -m | --model_type )     shift
+                                model_type=$1
                                 ;;
     esac
     shift
@@ -14,7 +10,7 @@ done
 
 for VARIABLE in 0 1 2 3
 do
-        python train.py -g $VARIABLE -s -i $iterations -m $model_type --seed ($VARIABLE \* 1000 + $seed) &
+        python train.py -g $VARIABLE -s -m $model_type --seed $VARIABLE &
         disown %1
         sleep 5
 
