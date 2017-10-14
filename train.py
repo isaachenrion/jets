d6d3562bfdc1dd7e922f4b68b9a66e5c2702e0dc
isaachenrion <isaachenrion@gmail.com>
2017-10-14 19:25:44 -0400

    heres hoping

--- train.py
+++ train.py
@@ -119,6 +119,7 @@ def train():
 
     pid = os.getpid()
     logging.warning("\tPID = {}".format(pid))
+    logging.warning("\tTraining on GPU: {}".format(torch.cuda.is_available()))
 
     ''' EMAIL '''
     '''----------------------------------------------------------------------- '''
@@ -162,7 +163,7 @@ def train():
     logging.warning("Splitting into train and validation...")
 
     X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=min(5000, len(X) // 5))
-    #X_valid, y_valid, w_valid = crop(X_valid, y_valid)
+    X_valid, y_valid, w_valid = crop(X_valid, y_valid)
     logging.warning("\ttrain size = %d" % len(X_train))
     logging.warning("\tvalid size = %d" % len(X_valid))
 
@@ -241,50 +242,26 @@ def train():
                 tl = unwrap(loss(model(X_var), y_var)); train_loss.append(tl)
                 X = unwrap_X(X_var); y = unwrap(y_var)
 
-                #Xv, yv = X_valid[idx], y_valid[idx]
-                #X_var = wrap_X(Xv); y_var = wrap(yv)
-                #y_pred = model(X_var)
-                #vl = unwrap(loss(y_pred, y_var)); valid_loss.append(vl)
-                #X = unwrap_X(X_var); y = unwrap(y_var); y_pred = unwrap(y_pred)
-
                 Xv, yv = X_valid[offset:offset+args.batch_size], y_valid[offset:offset+args.batch_size]
                 X_var = wrap_X(Xv); y_var = wrap(yv)
                 y_pred = model(X_var)
                 vl = unwrap(loss(y_pred, y_var)); valid_loss.append(vl)
                 Xv = unwrap_X(X_var); yv = unwrap(y_var); y_pred = unwrap(y_pred)
-                #yy.append(y); yy_pred.append(y_pred)
-
                 yy.append(y); yy_pred.append(y_pred)
-                #X_valid[idx] = unwrap_X(X)
 
                 offset+=args.batch_size
 
-            # last batch
-            #if len(X_train[offset:]) > 0:
-            #    Xt, yt = X_train[offset:], y_train[offset:]
-            #    X_var = wrap_X(Xt); y_var = wrap(yt)
-            #    tl = unwrap(loss(model(X_var), y_var)); train_loss.append(tl)
-            #    X = unwrap_X(X_var); y = unwrap(y_var)
-            #if len(X_valid[offset:]) > 0:
-            #    Xv, yv = X_valid[offset:], y_valid[offset:]
-            #    X_var = wrap_X(Xv); y_var = wrap(yv)
-            #    y_pred = model(X_var)
-            #    vl = unwrap(loss(y_pred, y_var)); valid_loss.append(vl)
-            #    X = unwrap_X(X_var); y = unwrap(y_var); y_pred = unwrap(y_pred)
-            #    yy.append(y); yy_pred.append(y_pred)
-            #except:
 
             train_loss = np.mean(np.array(train_loss))
             valid_loss = np.mean(np.array(valid_loss))
             yy = np.concatenate(yy, 0)
             yy_pred = np.concatenate(yy_pred, 0)
 
-            roc_auc = roc_auc_score(yy, yy_pred)
-            inv_fpr = 0
-            #fpr, tpr, _ = roc_curve(yy, yy_pred, sample_weight=w_valid)
-            #inv_fpr = inv_fpr_at_tpr_equals_half(tpr, fpr)
-            #if np.isnan(inv_fpr):
-            #    logging.warning("NaN in 1/FPR\n"+out_str)
+            roc_auc = roc_auc_score(yy, yy_pred, sample_weight=w_valid)
+            fpr, tpr, _ = roc_curve(yy, yy_pred, sample_weight=w_valid)
+            inv_fpr = inv_fpr_at_tpr_equals_half(tpr, fpr)
+            if np.isnan(inv_fpr):
+                logging.warning("NaN in 1/FPR\n"+out_str)
             model.train()
 
             if roc_auc > best_score[0]:
@@ -294,12 +271,14 @@ def train():
 
             logging.info(
                 "%5d\t~loss(train)=%.4f\tloss(valid)=%.4f"
-                "\troc_auc(valid)=%.4f\tbest_roc_auc(valid)=%.4f" % (
+                "\troc_auc(valid)=%.4f\tbest_roc_auc(valid)=%.4f"
+                "\t(1/fpr @ tpr=0.5)(valid) = " % (
                     iteration,
                     train_loss,
                     valid_loss,
                     roc_auc,
-                    best_score[0]))
+                    best_score[0],
+                    inv_fpr))
     ''' TRAINING '''
     '''----------------------------------------------------------------------- '''
     try:
