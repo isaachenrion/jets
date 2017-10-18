cd576bca095b30b5138e8dadbf0a9447972164ca
isaachenrion <isaachenrion@gmail.com>
2017-10-18 16:20:35 -0400

    array slurm

--- arrayslurm.s
+++ arrayslurm.s
@@ -8,7 +8,6 @@
 #SBATCH --mail-type=END
 #SBATCH --mail-user=henrion@nyu.edu
 #SBATCH --output=slurm_%j.out
-##SBATCH --array=1-10
 
 module purge
 SRCDIR=$HOME/jets
@@ -16,19 +15,11 @@ DATA_DIR=$SCRATCH/data/w-vs-qcd/pickles
 cd $SRCDIR
 source activate jets
 model_type=5
-N=3
-COUNTER=0
-
-printf 'N = %s, Model = %s, DATA_DIR = %s\n' $N $model_type $DATA_DIR
-while [  $COUNTER -lt $N ];
-do
-  let 'SEED = COUNTER * 10000'
-  printf 'Running with seed = %s\n' $SEED
-  python train.py --debug --data_dir $DATA_DIR  -v -m $model_type --seed $SEED -g 1 &
-  disown %1
-  sleep 5
-  let COUNTER=COUNTER+1
-done
+COUNTER=$SLURM_ARRAY_TASK_ID
+let 'SEED = COUNTER * 10000'
+python train.py --data_dir $DATA_DIR -m $model_type --seed $SEED -g 1 &
+disown %1
+sleep 5
 
 ##./slurm_run.sh -d $DATA_DIR -m 5 -n 3
 #########
