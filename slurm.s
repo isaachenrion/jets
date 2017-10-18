fc3ad2634bb1fce6d704c9c960f103295fe59074
isaachenrion <isaachenrion@gmail.com>
2017-10-18 11:03:19 -0400

    slurm bug

--- slurm.s
+++ slurm.s
@@ -10,11 +10,11 @@
 #SBATCH --output=slurm_%j.out
 
 module purge
-
+SRCDIR = $HOME/jets
+cd $SRCDIR 
 module load python3/intel/3.6.3
 module load pytorch/0.2.0_1
 module load scikit-learn/intel/0.18.1
-jets
 chmod +x ./slurm_run.sh
 ./slurm_run.sh -d $SCRATCH -m 5 -n 1
 
