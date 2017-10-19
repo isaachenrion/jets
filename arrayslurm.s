94043f65f955835d22269b35dae522fe6e61c61e
isaachenrion <isaachenrion@gmail.com>
2017-10-19 14:36:20 -0400

    slurm

--- arrayslurm.s
+++ arrayslurm.s
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=5
+#SBATCH --cpus-per-task=1
 #SBATCH --time=01:00:00
 #SBATCH --mem=20GB
 #SBATCH --job-name=jets-experiment
