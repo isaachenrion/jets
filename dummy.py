a34f018536baf71712c4d1889bc56929fb00d07c
isaachenrion <isaachenrion@gmail.com>
2017-10-24 17:31:54 -0400

    debug

--- dummy.py
+++ dummy.py
@@ -1,4 +1,9 @@
 import time
-for k in range(120):
-   time.sleep(1)
-   print('{} seconds'.format(k+1))
+import torch
+
+linear = torch.nn.Linear(100,200)
+linear.cuda()
+
+for k in range(6000):
+  time.sleep(1)
+  print('{} seconds'.format(k+1))
