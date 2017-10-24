de1cf39a858a51a747fafb8ec76997b2cf9c5467
isaachenrion <isaachenrion@gmail.com>
2017-10-24 17:20:37 -0400

    debug

--- slurm.s
+++ slurm.s
@@ -27,6 +27,7 @@ GPU=0
 COUNTER=0
 BATCH_SIZE=10
 
+printf 'CUDA VISIBLE DEVICES : %s' $CUDA_VISIBLE_DEVICES
 printf 'N = %s, Model = %s, DATA_DIR = %s\n' $N $MODEL_TYPE $DATA_DIR
 while [  $COUNTER -lt $N_RUNS ];
 do
