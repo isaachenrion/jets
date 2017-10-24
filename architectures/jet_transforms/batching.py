fix softmax

architectures/jet_transforms/batching.py
@@ -8,9 +8,12 @@ def pad_batch(jets):
     jets_padded = []
     for jet in jet_contents:
         if jet.size()[0] < biggest_jet_size:
-            zeros = Variable(torch.zeros(biggest_jet_size - jet.size()[0], jet.size()[1]))
-            if torch.cuda.is_available(): zeros = zeros.cuda()
-            jets_padded.append(torch.cat((jet, zeros), 0))
+            padding = torch.zeros(biggest_jet_size - jet.size()[0], jet.size()[1])
+            padding = Variable(padding)
+            if torch.cuda.is_available(): padding = padding.cuda()
+            padding = torch.cat((padding, torch.zeros(padding.size()[0], 1)), 1)
+            jet = torch.cat((jet, torch.ones(jet.size()[0], 1)), 1)
+            jets_padded.append(torch.cat((jet, padding), 0))
         else:
             jets_padded.append(jet)
     jets_padded = torch.stack(jets_padded, 0)
@@ -196,16 +199,22 @@ def batch_leaves(jets):
     leaves = [torch.stack([jet[i] for i in outers], 0) for jet, outers in zip(jet_contents, batch_outers)]
 
     biggest_jet_size = max(len(jet) for jet in leaves)
+    original_sizes = [len(jet) for jet in leaves]
     jets_padded = []
     for jet in leaves:
         if jet.size()[0] < biggest_jet_size:
-            zeros = Variable(torch.zeros(biggest_jet_size - jet.size()[0], jet.size()[1]))
-            if torch.cuda.is_available(): zeros = zeros.cuda()
-            jets_padded.append(torch.cat((jet, zeros), 0))
+            padding = torch.zeros(biggest_jet_size - jet.size()[0], jet.size()[1])
+            padding = Variable(padding)
+            if torch.cuda.is_available(): padding = padding.cuda()
+            padding = torch.cat((padding, torch.zeros(padding.size()[0], 1)), 1)
+            jet = torch.cat((jet, torch.ones(jet.size()[0], 1)), 1)
+            jets_padded.append(torch.cat((jet, padding), 0))
+
         else:
+            jet = torch.cat((jet, torch.ones(jet.size()[0], 1)), 1)
             jets_padded.append(jet)
     jets_padded = torch.stack(jets_padded, 0)
-    return jets_padded
+    return jets_padded, original_sizes
 
 def trees_as_adjacency_matrices(jets):
     def tree_as_adjacency_matrix(jet):

architectures/jet_transforms/message_net/adjacency.py
@@ -7,10 +7,42 @@ class AdaptiveAdjacencyMatrix(nn.Module):
     def __init__(self, hidden):
         super().__init__()
         self.edge_embedding = nn.Linear(hidden, 1)
+        self.softmax = PaddedMatrixSoftmax()
 
-    def forward(self, h):
+    def forward(self, h, original_sizes):
         shp = h.size()
         h_l = h.view(shp[0], shp[1], 1, shp[2])
         h_r = h.view(shp[0], 1, shp[1], shp[2])
-        A = F.softmax(self.edge_embedding(h_l + h_r)).squeeze(-1)
+        A = self.edge_embedding(h_l + h_r)
+        #A = F.softmax(A).squeeze(-1)
+        A = self.softmax(A, original_sizes)
         return A
+
+class PaddedMatrixSoftmax(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, matrix, original_sizes):
+        '''
+        Inputs:
+            matrix <- (batch_size) * M * M tensor that has been padded
+            original_sizes <- list of length batch_size, containing the original sizes before padding
+        Output:
+            S <- (batch_size) * M * M tensor, where S[n, i] is a probability distribution over the
+                values 1, ..., M. The softmax is taken over each row of the
+                matrix, and the padded values have been assigned probability 0.
+        '''
+        matrix_max = torch.max(matrix, 2, keepdim=True)
+        exp_matrix = torch.exp(matrix - matrix_max)
+        S = exp_matrix / torch.sum(exp_matrix, 2, keepdim=True)
+        mask = torch.ones(matrix.size())
+        for i, size in enumerate(original_sizes):
+            if size < matrix.size()[1]:
+                mask[i, size:, :].fill_(0)
+                mask[i, :, size:].fill_(0)
+
+        S = S * Variable(mask)
+        Z = S.sum(2, keepdim=True) + 1e-10
+        S = S / Z
+        S = S.squeeze(-1)
+        return S

architectures/jet_transforms/message_net/message_net.py
@@ -19,8 +19,8 @@ class MPNNTransform(nn.Module):
         self.activation = F.tanh
         self.hidden = hidden
         self.features = features
-        self.embedding = nn.Linear(features, hidden)
-        self.vertex_update = GRUUpdate(hidden, hidden, features)
+        self.embedding = nn.Linear(features + 1, hidden)
+        self.vertex_update = GRUUpdate(hidden, hidden, features + 1)
         self.message = DTNNMessage(hidden, hidden, 0)
         self.readout = DTNNReadout(hidden, hidden)
         if adjacency_matrix is not None:
@@ -31,12 +31,12 @@ class MPNNTransform(nn.Module):
 
     def forward(self, jets, returextras=False):
         if self.leaves:
-            jets = batch_leaves(jets)
+            jets, original_sizes = batch_leaves(jets)
         else:
             jets = pad_batch(jets)
         h = self.activation(self.embedding(jets))
         for i in range(self.iters):
-            A = self.adjacency_matrix(h)
+            A = self.adjacency_matrix(h, original_sizes)
             h = self.message_passing(h, jets, A)
         out = self.readout(h)
         if returextras: