be42fdb09ad3de8c7cdb2d41d9032fbe66bfab72
isaachenrion <isaachenrion@gmail.com>
2017-10-25 10:04:29 -0400

    debug

--- architectures/jet_transforms/message_net/adjacency.py
+++ architectures/jet_transforms/message_net/adjacency.py
@@ -9,13 +9,13 @@ class AdaptiveAdjacencyMatrix(nn.Module):
         self.edge_embedding = nn.Linear(hidden, 1)
         self.softmax = PaddedMatrixSoftmax()
 
-    def forward(self, h, original_sizes):
+    def forward(self, h, mask):
         shp = h.size()
         h_l = h.view(shp[0], shp[1], 1, shp[2])
         h_r = h.view(shp[0], 1, shp[1], shp[2])
         A = self.edge_embedding(h_l + h_r).squeeze(-1)
-        A = F.softmax(A).squeeze(-1)
-        #A = self.softmax(A, original_sizes)
+        #A = F.softmax(A).squeeze(-1)
+        A = self.softmax(A, mask)
         return A
 
 class PaddedMatrixSoftmax(nn.Module):
@@ -32,10 +32,10 @@ class PaddedMatrixSoftmax(nn.Module):
                 values 1, ..., M. The softmax is taken over each row of the
                 matrix, and the padded values have been assigned probability 0.
         '''
-        matrix_max, _ = torch.max(matrix, 2, keepdim=True)
-        exp_matrix = torch.exp(matrix - matrix_max)
-        S = exp_matrix / torch.sum(exp_matrix, 2, keepdim=True)
-
+        #matrix_max, _ = torch.max(matrix, 2, keepdim=True)
+        #exp_matrix = torch.exp(matrix - matrix_max)
+        #S = exp_matrix / torch.sum(exp_matrix, 2, keepdim=True)
+        S = F.softmax(matrix)
         S = S * mask
         Z = S.sum(2, keepdim=True) + 1e-10
         S = S / Z
