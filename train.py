CHECKPOINT fixed softmax problem in MPNN

train.py
@@ -109,207 +109,202 @@ if args.n_train <= 5 * args.n_valid and args.n_train > 0:
 args.recipient = RECIPIENT
 args.leaves = not args.not_leaves
 def train(args):
-    try:
-        _, Transform, model_type = TRANSFORMS[args.model_type]
-        eh = ExperimentHandler(args, os.path.join(MODELS_DIR,model_type))
-        signal_handler = eh.signal_handler
 
-        ''' DATA '''
-        '''----------------------------------------------------------------------- '''
-        logging.warning("Loading data...")
-        if args.debug and False:
-            for k in range(15):
-                time.sleep(1)
-                logging.info('before load tf: {} seconds'.format(k+1))
+    _, Transform, model_type = TRANSFORMS[args.model_type]
+    eh = ExperimentHandler(args, os.path.join(MODELS_DIR,model_type))
+    signal_handler = eh.signal_handler
 
-        tf = load_tf(args.data_dir, "{}-train.pickle".format(args.filename))
+    ''' DATA '''
+    '''----------------------------------------------------------------------- '''
+    logging.warning("Loading data...")
+    if args.debug and False:
+        for k in range(15):
+            time.sleep(1)
+            logging.info('before load tf: {} seconds'.format(k+1))
 
-        if args.debug and False:
-            for k in range(15):
-                time.sleep(1)
-                logging.info('before load_data: {} seconds'.format(k+1))
+    tf = load_tf(args.data_dir, "{}-train.pickle".format(args.filename))
 
-        X, y = load_data(args.data_dir, "{}-train.pickle".format(args.filename))
+    if args.debug and False:
+        for k in range(15):
+            time.sleep(1)
+            logging.info('before load_data: {} seconds'.format(k+1))
 
-        logging.warning("Memory usage = {}".format(0))
-        if args.debug and False:
-            for k in range(15):
-                time.sleep(1)
-                logging.info('before tf transform: {} seconds'.format(k+1))
+    X, y = load_data(args.data_dir, "{}-train.pickle".format(args.filename))
 
-        for ij, jet in enumerate(X):
-            jet["content"] = tf.transform(jet["content"])
+    logging.warning("Memory usage = {}".format(0))
+    if args.debug and False:
+        for k in range(15):
+            time.sleep(1)
+            logging.info('before tf transform: {} seconds'.format(k+1))
 
-        logging.warning("After transform: memory usage = {}".format(eh.usage()))
+    for ij, jet in enumerate(X):
+        jet["content"] = tf.transform(jet["content"])
 
-        if args.n_train > 0:
-            indices = torch.randperm(len(X)).numpy()[:args.n_train]
-            X = [X[i] for i in indices]
-            y = y[indices]
+    logging.warning("After transform: memory usage = {}".format(eh.usage()))
 
-        logging.warning("Splitting into train and validation...")
+    if args.n_train > 0:
+        indices = torch.randperm(len(X)).numpy()[:args.n_train]
+        X = [X[i] for i in indices]
+        y = y[indices]
 
-        X_train, X_valid_uncropped, y_train, y_valid_uncropped = train_test_split(X, y, test_size=args.n_valid, random_state=0)
-        logging.warning("\traw train size = %d" % len(X_train))
-        logging.warning("\traw valid size = %d" % len(X_valid_uncropped))
+    logging.warning("Splitting into train and validation...")
 
-        X_valid, y_valid, cropped_indices, w_valid = crop(X_valid_uncropped, y_valid_uncropped, return_cropped_indices=True)
+    X_train, X_valid_uncropped, y_train, y_valid_uncropped = train_test_split(X, y, test_size=args.n_valid, random_state=0)
+    logging.warning("\traw train size = %d" % len(X_train))
+    logging.warning("\traw valid size = %d" % len(X_valid_uncropped))
 
-        # add cropped indices to training data
-        if not args.dont_add_cropped:
-            X_train.extend([x for i, x in enumerate(X_valid_uncropped) if i in cropped_indices])
-            y_train = [y for y in y_train]
-            y_train.extend([y for i, y in enumerate(y_valid_uncropped) if i in cropped_indices])
-            y_train = np.array(y_train)
-        logging.warning("\tfinal train size = %d" % len(X_train))
-        logging.warning("\tfinal valid size = %d" % len(X_valid))
+    X_valid, y_valid, cropped_indices, w_valid = crop(X_valid_uncropped, y_valid_uncropped, return_cropped_indices=True)
 
-        ''' MODEL '''
-        '''----------------------------------------------------------------------- '''
-        # Initialization
-        logging.info("Initializing model...")
-        Predict = PredictFromParticleEmbedding
-        if args.load is None:
-            model_kwargs = {
-                'features': args.features,
-                'hidden': args.hidden,
-                'iters': args.iters,
-                'leaves': args.leaves,
+    # add cropped indices to training data
+    if not args.dont_add_cropped:
+        X_train.extend([x for i, x in enumerate(X_valid_uncropped) if i in cropped_indices])
+        y_train = [y for y in y_train]
+        y_train.extend([y for i, y in enumerate(y_valid_uncropped) if i in cropped_indices])
+        y_train = np.array(y_train)
+    logging.warning("\tfinal train size = %d" % len(X_train))
+    logging.warning("\tfinal valid size = %d" % len(X_valid))
+
+    ''' MODEL '''
+    '''----------------------------------------------------------------------- '''
+    # Initialization
+    logging.info("Initializing model...")
+    Predict = PredictFromParticleEmbedding
+    if args.load is None:
+        model_kwargs = {
+            'features': args.features,
+            'hidden': args.hidden,
+            'iters': args.iters,
+            'leaves': args.leaves,
+        }
+        model = Predict(Transform, **model_kwargs)
+        settings = {
+            "transform": Transform,
+            "predict": Predict,
+            "model_kwargs": model_kwargs,
+            "step_size": args.step_size,
+            "args": args,
             }
-            model = Predict(Transform, **model_kwargs)
-            settings = {
-                "transform": Transform,
-                "predict": Predict,
-                "model_kwargs": model_kwargs,
-                "step_size": args.step_size,
-                "args": args,
-                }
-        else:
-            with open(os.path.join(args.load, 'settings.pickle'), "rb") as f:
-                settings = pickle.load(f, encoding='latin-1')
-                Transform = settings["transform"]
-                Predict = settings["predict"]
-                model_kwargs = settings["model_kwargs"]
+    else:
+        with open(os.path.join(args.load, 'settings.pickle'), "rb") as f:
+            settings = pickle.load(f, encoding='latin-1')
+            Transform = settings["transform"]
+            Predict = settings["predict"]
+            model_kwargs = settings["model_kwargs"]
 
-            with open(os.path.join(args.load, 'model_state_dict.pt'), 'rb') as f:
-                state_dict = torch.load(f)
-                model = PredictFromParticleEmbedding(Transform, **model_kwargs)
-                model.load_state_dict(state_dict)
+        with open(os.path.join(args.load, 'model_state_dict.pt'), 'rb') as f:
+            state_dict = torch.load(f)
+            model = PredictFromParticleEmbedding(Transform, **model_kwargs)
+            model.load_state_dict(state_dict)
 
-            if args.restart:
-                args.step_size = settings["step_size"]
+        if args.restart:
+            args.step_size = settings["step_size"]
 
-        logging.warning(model)
-        out_str = 'Number of parameters: {}'.format(sum(np.prod(p.data.numpy().shape) for p in model.parameters()))
-        logging.warning(out_str)
+    logging.warning(model)
+    out_str = 'Number of parameters: {}'.format(sum(np.prod(p.data.numpy().shape) for p in model.parameters()))
+    logging.warning(out_str)
 
-        if torch.cuda.is_available():
-            logging.warning("Moving model to GPU")
-            if args.debug and False:
-                for k in range(30):
-                    time.sleep(1)
-                    logging.info('{} seconds'.format(k+1))
-            model.cuda()
+    if torch.cuda.is_available():
+        logging.warning("Moving model to GPU")
+        if args.debug and False:
+            for k in range(30):
+                time.sleep(1)
+                logging.info('{} seconds'.format(k+1))
+        model.cuda()
 
-        logging.warning("Moved model to GPU")
-        signal_handler.set_model(model)
+    logging.warning("Moved model to GPU")
+    signal_handler.set_model(model)
 
-        ''' OPTIMIZER AND LOSS '''
-        '''----------------------------------------------------------------------- '''
-        logging.info("Building optimizer...")
-        optimizer = Adam(model.parameters(), lr=args.step_size)
-        scheduler = lr_scheduler.ExponentialLR(optimizer, gamma=args.decay)
-        #scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.5)
+    ''' OPTIMIZER AND LOSS '''
+    '''----------------------------------------------------------------------- '''
+    logging.info("Building optimizer...")
+    optimizer = Adam(model.parameters(), lr=args.step_size)
+    scheduler = lr_scheduler.ExponentialLR(optimizer, gamma=args.decay)
+    #scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.5)
 
-        n_batches = int(np.ceil(len(X_train) / args.batch_size))
-        best_score = [-np.inf]  # yuck, but works
-        best_model_state_dict = copy.deepcopy(model.state_dict())
+    n_batches = int(np.ceil(len(X_train) / args.batch_size))
+    best_score = [-np.inf]  # yuck, but works
+    best_model_state_dict = copy.deepcopy(model.state_dict())
 
-        def loss(y_pred, y):
-            l = log_loss(y, y_pred.squeeze(1)).mean()
-            return l
+    def loss(y_pred, y):
+        l = log_loss(y, y_pred.squeeze(1)).mean()
+        return l
 
 
-            ''' VALIDATION '''
-        '''----------------------------------------------------------------------- '''
-        def callback(epoch, iteration, model):
+        ''' VALIDATION '''
+    '''----------------------------------------------------------------------- '''
+    def callback(epoch, iteration, model):
 
-            if iteration % args.eval_every == 0:
-                model.eval()
+        if iteration % args.eval_every == 0:
+            model.eval()
 
-                offset = 0; train_loss = []; valid_loss = []
-                yy, yy_pred = [], []
-                for i in range(len(X_valid) // args.batch_size):
-                    idx = slice(offset, offset+args.batch_size)
-                    Xt, yt = X_train[idx], y_train[idx]
-                    X_var = wrap_X(Xt); y_var = wrap(yt)
-                    tl = unwrap(loss(model(X_var), y_var)); train_loss.append(tl)
-                    X = unwrap_X(X_var); y = unwrap(y_var)
-
-                    Xv, yv = X_valid[offset:offset+args.batch_size], y_valid[offset:offset+args.batch_size]
-                    X_var = wrap_X(Xv); y_var = wrap(yv)
-                    y_pred = model(X_var)
-                    vl = unwrap(loss(y_pred, y_var)); valid_loss.append(vl)
-                    Xv = unwrap_X(X_var); yv = unwrap(y_var); y_pred = unwrap(y_pred)
-                    yy.append(yv); yy_pred.append(y_pred)
-
-                    offset+=args.batch_size
-
-                train_loss = np.mean(np.array(train_loss))
-                valid_loss = np.mean(np.array(valid_loss))
-                yy = np.concatenate(yy, 0)
-                yy_pred = np.concatenate(yy_pred, 0)
-
-                logdict = dict(
-                    epoch=epoch,
-                    iteration=iteration,
-                    yy=yy,
-                    yy_pred=yy_pred,
-                    w_valid=w_valid,
-                    train_loss=train_loss,
-                    valid_loss=valid_loss,
-                    settings=settings,
-                    model=model
-                )
-                eh.log(**logdict)
-
-                scheduler.step(valid_loss)
-                model.train()
-
-        ''' TRAINING '''
-        '''----------------------------------------------------------------------- '''
-        eh.save(model, settings)
-        logging.warning("Training...")
-        iteration=1
-        for i in range(args.epochs):
-            logging.info("epoch = %d" % i)
-            logging.info("step_size = %.8f" % settings['step_size'])
-
-            for _ in range(n_batches):
-                iteration += 1
-                model.train()
-                optimizer.zero_grad()
-                start = torch.round(torch.rand(1) * (len(X_train) - args.batch_size)).numpy()[0].astype(np.int32)
-                idx = slice(start, start+args.batch_size)
-                X, y = X_train[idx], y_train[idx]
-                X_var = wrap_X(X); y_var = wrap(y)
-                l = loss(model(X_var), y_var)
-                l.backward()
-                optimizer.step()
+            offset = 0; train_loss = []; valid_loss = []
+            yy, yy_pred = [], []
+            for i in range(len(X_valid) // args.batch_size):
+                idx = slice(offset, offset+args.batch_size)
+                Xt, yt = X_train[idx], y_train[idx]
+                X_var = wrap_X(Xt); y_var = wrap(yt)
+                tl = unwrap(loss(model(X_var), y_var)); train_loss.append(tl)
                 X = unwrap_X(X_var); y = unwrap(y_var)
 
-                callback(i, iteration, model)
+                Xv, yv = X_valid[offset:offset+args.batch_size], y_valid[offset:offset+args.batch_size]
+                X_var = wrap_X(Xv); y_var = wrap(yv)
+                y_pred = model(X_var)
+                vl = unwrap(loss(y_pred, y_var)); valid_loss.append(vl)
+                Xv = unwrap_X(X_var); yv = unwrap(y_var); y_pred = unwrap(y_pred)
+                yy.append(yv); yy_pred.append(y_pred)
 
-            scheduler.step()
-            settings['step_size'] = args.step_size * (args.decay) ** (i + 1)
+                offset+=args.batch_size
 
-        eh.finished()
+            train_loss = np.mean(np.array(train_loss))
+            valid_loss = np.mean(np.array(valid_loss))
+            yy = np.concatenate(yy, 0)
+            yy_pred = np.concatenate(yy_pred, 0)
 
-    except SystemExit as e:
-        logging.warning(e)
-        if not signal_handler.done:
-            signal_handler.crashed()
-        sys.exit()
+            logdict = dict(
+                epoch=epoch,
+                iteration=iteration,
+                yy=yy,
+                yy_pred=yy_pred,
+                w_valid=w_valid,
+                train_loss=train_loss,
+                valid_loss=valid_loss,
+                settings=settings,
+                model=model
+            )
+            eh.log(**logdict)
+
+            scheduler.step(valid_loss)
+            model.train()
+
+    ''' TRAINING '''
+    '''----------------------------------------------------------------------- '''
+    eh.save(model, settings)
+    logging.warning("Training...")
+    iteration=1
+    for i in range(args.epochs):
+        logging.info("epoch = %d" % i)
+        logging.info("step_size = %.8f" % settings['step_size'])
+
+        for _ in range(n_batches):
+            iteration += 1
+            model.train()
+            optimizer.zero_grad()
+            start = torch.round(torch.rand(1) * (len(X_train) - args.batch_size)).numpy()[0].astype(np.int32)
+            idx = slice(start, start+args.batch_size)
+            X, y = X_train[idx], y_train[idx]
+            X_var = wrap_X(X); y_var = wrap(y)
+            l = loss(model(X_var), y_var)
+            l.backward()
+            optimizer.step()
+            X = unwrap_X(X_var); y = unwrap(y_var)
+
+            callback(i, iteration, model)
+
+        scheduler.step()
+        settings['step_size'] = args.step_size * (args.decay) ** (i + 1)
+
+    eh.finished()
+
 
 
 if __name__ == "__main__":