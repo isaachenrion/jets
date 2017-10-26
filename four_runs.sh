edd9de1c6c35ec9979a34678c06676fecd2b6b7f
isaachenrion <isaachenrion@gmail.com>
2017-10-26 15:33:53 -0400

    debug

--- four_runs.sh
+++ four_runs.sh
@@ -10,7 +10,7 @@ while [ "$1" != "" ]; do
     esac
 done
 printf "Launching a job on each GPU with model type %s, base seed is %s\n" $model_type $base_seed
-batch_size=256
+batch_size=100
 step_size=0.001
 decay=0.96
 epochs=100
