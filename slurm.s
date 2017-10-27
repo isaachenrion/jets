fbab432de86b51f28c7dafc2b5c804a8cdde9f3f
isaachenrion <isaachenrion@gmail.com>
2017-10-26 20:34:11 -0400

    debug

--- slurm.s
+++ slurm.s
@@ -3,7 +3,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --time=30:00:00
-#SBATCH --mem=20GB
+#SBATCH --mem=30GB
 #SBATCH --job-name=jets-experiment
 #SBATCH --mail-type=END
 #SBATCH --mail-user=henrion@nyu.edu
