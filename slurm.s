935ab01949015334d282ad50a3718f71f337478e
isaachenrion <isaachenrion@gmail.com>
2017-10-28 15:02:56 -0400

    debug

--- slurm.s
+++ slurm.s
@@ -9,7 +9,7 @@
 #SBATCH --mail-user=henrion@nyu.edu
 #SBATCH --output=slurm_%j.out
 #SBATCH --error=slurm_%j.err
-##SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:1
 #SBATCH --array=1-10
 module purge
 
@@ -19,19 +19,19 @@ cd $SRCDIR
 source activate jets
 
 DATA_DIR=$SCRATCH/data/w-vs-qcd/pickles
-MODEL_TYPE=7
+MODEL_TYPE=5
 EPOCHS=20
 N=-1
 ITERS=1
 GPU=0
 BATCH_SIZE=100
 STEP_SIZE=0.001
-EXTRA_TAG=$SLURM_ARRAY_TASK_ID
-MODEL_DIR=$SRCDIR/models/MPNN/set/Oct-26/
-MODEL_DIR=$MODEL_DIR$SLURM_ARRAY_TASK_ID
+##EXTRA_TAG=$SLURM_ARRAY_TASK_ID
+##MODEL_DIR=$SRCDIR/models/MPNN/set/Oct-26/
+##MODEL_DIR=$MODEL_DIR$SLURM_ARRAY_TASK_ID
 ##let 'SEED=SLURM_ARRAY_TASK_ID * 1000'
 printf 'CUDA VISIBLE DEVICES : %s\n' $CUDA_VISIBLE_DEVICES
 printf 'N = %s, Model = %s, DATA_DIR = %s\n' $N $MODEL_TYPE $DATA_DIR
 sleep 5
-##python train.py --extra_tag $EXTRA_TAG --iters $ITERS --step_size $STEP_SIZE -b $BATCH_SIZE -s -m $MODEL_TYPE --data_dir $DATA_DIR  -e $EPOCHS -n $N -g $GPU
-python train.py --extra_tag $EXTRA_TAG -b $BATCH_SIZE -s -l $MODEL_DIR -r --data_dir $DATA_DIR  -m $MODEL_TYPE -e $EPOCHS -n $N -g $GPU
+python train.py --extra_tag $EXTRA_TAG --iters $ITERS --step_size $STEP_SIZE -b $BATCH_SIZE -s -m $MODEL_TYPE --data_dir $DATA_DIR  -e $EPOCHS -n $N -g $GPU
+##python train.py --extra_tag $EXTRA_TAG -b $BATCH_SIZE -s -l $MODEL_DIR -r --data_dir $DATA_DIR  -m $MODEL_TYPE -e $EPOCHS -n $N -g $GPU
