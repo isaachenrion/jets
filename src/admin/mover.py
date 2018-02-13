eccd0a3224690e4b3e3c383e57a5f2c1ca42c840
isaachenrion <isaachenrion@gmail.com>
2018-02-13 14:54:10 -0500

    debug

---
+++ src/admin/mover.py
@@ -0,0 +1,33 @@
+import shutil
+import os
+import logging
+
+from ..misc.constants import FINISHED_MODELS_DIR, DEBUG_MODELS_DIR, KILLED_MODELS_DIR, INTERRUPTED_MODELS_DIR
+
+class Mover:
+    def __init__(self, root_dir, intermediate_dir, leaf_dir):
+        self.current_root = root_dir
+        self.leaf_dir = leaf_dir
+        self.intermediate_dir = intermediate_dir
+
+    def move_to_folder(self, folder):
+        src = os.path.join(self.current_root, self.intermediate_dir, self.leaf_dir)
+        dst = os.path.join(folder, self.intermediate_dir, self.leaf_dir)
+        intermediate_path = os.path.join(folder, self.intermediate_dir)
+        if not os.path.exists(intermediate_path):
+            os.makedirs(intermediate_path)
+        shutil.move(src, dst)
+        logging.info('Moved model directory to {}'.format(dst))
+        self.current_root = folder
+
+    def move_to_finished(self):
+        self.move_to_folder(FINISHED_MODELS_DIR)
+
+    def move_to_debug(self):
+        self.move_to_folder(DEBUG_MODELS_DIR)
+
+    def move_to_killed(self):
+        self.move_to_folder(KILLED_MODELS_DIR)
+
+    def move_to_interrupted(self):
+        self.move_to_folder(INTERRUPTED_MODELS_DIR)
---
+++ src/architectures/jet_transforms/transformer/transformer_transform.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from .transformer import Transformer
+
+from ....architectures.readout import construct_readout
+from ....architectures.embedding import construct_embedding
+
+class TransformerTransform(nn.Module):
+    def __init__(self,
+        features=None,
+        hidden=None,
+        n_heads=None,
+        n_layers=None,
+        readout=None,
+        **kwargs
+        ):
+        super().__init__()
+
+        self.embedding = construct_embedding('simple', features + 1, hidden, act='relu')
+        self.readout = construct_readout(readout, hidden, hidden)
+        self.transformer = Transformer(hidden, n_heads, n_layers, **kwargs)
+
+    def forward(self, jets, **kwargs):
+        h = self.embedding(jets)
+        h = self.transformer(h)
+        out = self.readout(h)
+        return out, None
