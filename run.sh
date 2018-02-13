08e1a4af0e3697294c6d8d28edd683d91b8d4377
isaachenrion <isaachenrion@gmail.com>
2018-02-13 13:35:31 -0500

    debug

--- run.sh
+++ run.sh
@@ -5,4 +5,9 @@ read SRCDIR DATADIR GPU QOS < <(bash misc/paths.sh)
 SLURMARGS="$SRCDIR $SLURMARGS --data_dir $DATADIR"
 RES=$(sbatch --parsable --array=1-$1 --gres=$GPU train.s $SLURMARGS)
 echo $RES
-sbatch --dependency=afterok:${RES} summarize_training.s ${RES}
+MODEL_RUNDIR=$(find models/running $RES)
+MODEL_OUTDIR=$(find models/finished $RES)
+echo $MODEL_RUNDIR
+echo $MODEL_OUTDIR
+
+sbatch --dependency=afterok:${RES} summarize_training.s $MODEL_OUTDIR
