c09f45aad907e61c1bb5670e8419cd816c5ae563
isaachenrion <isaachenrion@gmail.com>
2018-02-13 13:20:28 -0500

    debug

--- run.sh
+++ run.sh
@@ -1,10 +1,8 @@
-#SUBMISSION_OUT=$(sbatch --array=1-$1 train-hpc.s $SLURMARGS)
-#echo $SUBMISSION_OUT
 SLURMARGS="${@:2}"
 
 read SRCDIR DATADIR GPU QOS < <(bash misc/paths.sh)
 
 SLURMARGS="$SRCDIR $SLURMARGS --data_dir $DATADIR"
-RES=$(sbatch --parsable --array=1-$1 --gres=$GPU --qos=$QOS train.s $SLURMARGS)
+RES=$(sbatch --parsable --array=1-$1 --gres=$GPU train.s $SLURMARGS)
 echo $RES
 sbatch --dependency=afterok:${RES} summarize_training.s ${RES}
