f8ab0e153862d0335a7cd5aa106a98ab3cb09d4c
isaachenrion <isaachenrion@gmail.com>
2018-02-13 13:16:36 -0500

    debug

--- run.sh
+++ run.sh
@@ -6,4 +6,5 @@ read SRCDIR DATADIR GPU QOS < <(bash misc/paths.sh)
 
 SLURMARGS="$SRCDIR $SLURMARGS --data_dir $DATADIR"
 RES=$(sbatch --parsable --array=1-$1 --gres=$GPU --qos=$QOS train.s $SLURMARGS)
+echo $RES
 sbatch --dependency=afterok:${RES} summarize_training.s ${RES}
