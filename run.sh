82c909c810e3b171a254a2a93e275ce8aa9ecd81
isaachenrion <isaachenrion@gmail.com>
2018-02-21 11:53:08 -0500

    debug

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
 PYTHONARGS="${@:2}"
 
-read SRCDIR DATADIR GPU QOS < <(bash misc/paths.sh)
+read SRCDIR DATADIR GPU EXPT_TIME < <(bash misc/paths.sh)
 
-EXPT_TIME=48
 PYTHONARGS="$PYTHONARGS --data_dir $DATADIR --experiment_time $EXPT_TIME"
 RES=$(sbatch --time=$EXPT_TIME:00:00 --parsable --array=1-$1 --gres=$GPU train.s $PYTHONARGS)
 echo $RES
