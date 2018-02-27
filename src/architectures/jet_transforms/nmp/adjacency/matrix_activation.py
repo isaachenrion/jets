32c1b921265d3d587ebb121026eb2e9bd8272fce
isaachenrion <isaachenrion@gmail.com>
2018-02-27 12:03:09 -0500

    debug

--- src/architectures/jet_transforms/nmp/adjacency/matrix_activation.py
+++ src/architectures/jet_transforms/nmp/adjacency/matrix_activation.py
@@ -12,7 +12,7 @@ def padded_matrix_softmax(matrix, mask):
             values 1, ..., M. The softmax is taken over each row of the
             matrix, and the padded values have been assigned probability 0.
     '''
-    S = F.softmax(matrix.transpose(0, -1)).transpose(0, -1)
+    S = F.softmax(-matrix.transpose(0, -1)).transpose(0, -1)
     if mask is not None:
         S = S * mask
     Z = S.sum(2, keepdim=True) + 1e-10
