f468b077169f3a02ea483c0229c0df481996af43
isaachenrion <isaachenrion@gmail.com>
2018-03-07 14:35:36 -0500

    debug

--- grid.sh
+++ grid.sh
@@ -1,6 +1,4 @@
-
-S=" 10 --slurm --gpu 0 --lr .4,3,.3 --what who,when,where --slurm_array_job_id 4790119 --slurm_array_task_id=9 -a=[dm,phy,eye] --data_dir=/scratch/ih692/data"
-commands="$(python _grid.py $S)"
+commands="$(python _grid.py $@)"
 IFS=$'\n'
 commands=($commands)
 for cmd in ${commands[@]}
