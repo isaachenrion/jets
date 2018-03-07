081b2b04f25a148d811e70ba421fad2126922058
isaachenrion <isaachenrion@gmail.com>
2018-03-07 15:34:14 -0500

    debug

--- run.sh
+++ run.sh
@@ -7,8 +7,8 @@ echo $PYTHONARGS
 read SRCDIR DATA_DIR MODELS_DIR GPU EXPT_TIME END_DIR < <(bash misc/paths.sh)
 
 PYTHONARGS="$PYTHONARGS --data_dir $DATA_DIR --experiment_time $EXPT_TIME --models_dir $MODELS_DIR"
-echo sbatch --time=$EXPT_TIME:00:00 --parsable --array=1-$N_JOBS --gres=$GPU train.s $PYTHONARGS
-#RES=$(sbatch --time=$EXPT_TIME:00:00 --parsable --array=1-$N_JOBS --gres=$GPU train.s $PYTHONARGS)
+#echo sbatch --time=$EXPT_TIME:00:00 --parsable --array=1-$N_JOBS --gres=$GPU train.s $PYTHONARGS
+RES=$(sbatch --time=$EXPT_TIME:00:00 --parsable --array=1-$N_JOBS --gres=$GPU train.s $PYTHONARGS)
 echo $RES
 
 
