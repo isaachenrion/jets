d25942311f01c41a23bc91f9bec903ef5903badb
isaachenrion <isaachenrion@gmail.com>
2018-03-16 16:00:06 -0400

    debug

---
+++ grid-cpu.sh
@@ -0,0 +1,12 @@
+commands="$(python _grid.py "$@")"
+IFS=$'\n' read -r -d '' -a arr < <(printf '%s\0' "$commands")
+IFS=$'\n'
+#commands=("$commands")
+#echo ${commands[@]}
+for cmd in ${commands[@]}
+do
+    #echo ""
+    #echo $cmd
+    #cmd=( "$cmd" )
+    bash run-cpu.sh $cmd
+done
