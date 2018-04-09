f625d590018fccc76460d77f5b650c0990f0ff9a
isaachenrion <isaachenrion@gmail.com>
2018-04-09 15:59:39 -0400

    debug

--- sh/run.sh
+++ sh/run.sh
@@ -24,6 +24,6 @@ done
 
 read SRCDIR DATA_DIR MODELS_DIR GPU EXPT_TIME END_DIR < <(bash $SRCDIR/sh/paths.sh)
 PYTHONARGS="$PYTHONARGS --data_dir $DATA_DIR --experiment_time $EXPT_TIME --models_dir $MODELS_DIR"
-RES=$(sbatch --time=$EXPT_TIME:00:00 --parsable --array=1-$N_JOBS --gres=$GPU train.s $PYTHONARGS)
+RES=$(sbatch --time=$EXPT_TIME:00:00 --parsable --array=1-$N_JOBS --gres=$GPU $SRCDIR/sh/train.s $PYTHONARGS)
 echo $RES
 sbatch --dependency=afterok:${RES} $SRCDIR/sh/summarize_training.s $RES $MODELS_DIR $END_DIR
