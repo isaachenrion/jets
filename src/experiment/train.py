e5f308354d6059db66447e8fb09e12dfc732679f
isaachenrion <isaachenrion@gmail.com>
2018-04-13 13:58:47 -0400

    debug

--- src/experiment/train.py
+++ src/experiment/train.py
@@ -1,9 +1,9 @@
 
 def train(**kwargs):
     dataset = kwargs['data_args'].dataset
-    if dataset in ['w', 'pp', 'pbpb']:
+    if dataset in ['w', 'wp','pp', 'pbpb']:
         from src.jets.Training import Training
     elif dataset in ['protein']:
         from src.proteins.Training import Training
 
-    Training(**kwargs) 
+    Training(**kwargs)
