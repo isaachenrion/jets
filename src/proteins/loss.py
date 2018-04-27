370b0825fdc3c334ec260d11f5e4bfe1424e4583
isaachenrion <isaachenrion@gmail.com>
2018-04-27 14:25:43 -0400

    al models running on gpu using pytorch 4

--- src/proteins/loss.py
+++ src/proteins/loss.py
@@ -27,15 +27,10 @@ def nll(y_pred, y, y_mask, batch_mask):
     n = y_pred.shape[1]
     n_ = batch_mask.sum(1,keepdim=True)[:,:,0]
 
-    print(y.device)
-    print(y_mask.device)
-    print(batch_mask.device)
     dists = torch.tensor(distances(n)).to(y.device)
-    print(dists.device)
     dists = dists * batch_mask
-    x = torch.exp(-(n_.unsqueeze(1) - dists - 1)*0.01)
+    dists = torch.exp(-(n_.unsqueeze(1) - dists - 1)*0.01)
 
-    dists = (x)
     lossfn = torch.nn.NLLLoss(reduce=False)
     logprobs = stable_log(torch.stack([1-y_pred, y_pred], 1))
 
