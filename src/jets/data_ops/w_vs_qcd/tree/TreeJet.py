ae7404b35f40a3d76f3c90e1fb4052ac3c7f388e
isaachenrion <isaachenrion@gmail.com>
2018-05-06 12:28:42 -0400

    debug

--- src/jets/data_ops/w_vs_qcd/tree/TreeJet.py
+++ src/jets/data_ops/w_vs_qcd/tree/TreeJet.py
@@ -7,9 +7,10 @@ from .trees import BinaryTree
 class TreeJet(BinaryTree):
     def __init__(self, root_id, tree, tree_content, mass, pt, y, **kwargs):
         tree_content = torch.tensor(tree_content.astype(np.float32), device='cuda' if torch.cuda.is_available() else 'cpu').unsqueeze(1)
-        print(tree_content.dtype)
+        print(torch.cuda.is_available())
+        #print(tree_content.dtype)
         self.tree = BinaryTree.from_tree_content_and_node_ids(root_id, tree, tree_content)
-        print(self.tree.data.dtype)
+        #print(self.tree.data.dtype)
         self.mass = mass
         self.pt = pt
         self.y = y
