1dea4560d47f43e81dc8923fa42580b18d6e4b18
isaachenrion <isaachenrion@gmail.com>
2018-05-07 12:21:19 -0400

    debug

--- sh/slurm/test.s
+++ sh/slurm/test.s
@@ -32,4 +32,4 @@
 PYTHONARGS="$@"
 PYTHONARGS="$PYTHONARGS --slurm --gpu 0 --slurm_array_job_id $SLURM_ARRAY_JOB_ID --slurm_array_task_id $SLURM_ARRAY_TASK_ID"
 cd $SRCDIR/src/scripts/
-python eval.py $PYTHONARGS
+python test.py $PYTHONARGS
--- sh/test.sh
+++ sh/test.sh
@@ -13,4 +13,4 @@ do
 done
 read SRCDIR DATA_DIR MODELS_DIR GPU EXPT_TIME END_DIR < <(bash $SRCDIR/sh/paths.sh)
 PYTHONARGS="$PYTHONARGS --data_dir $DATA_DIR --experiment_time $EXPT_TIME --models_dir $MODELS_DIR --root_dir $SRCDIR/reports --email $SRCDIR/email_addresses.txt"
-RES=$(sbatch --time=$EXPT_TIME:00:00 --parsable --array=1-$N_JOBS --gres=$GPU $SRCDIR/sh/slurm/eval.s $PYTHONARGS)
+RES=$(sbatch --time=$EXPT_TIME:00:00 --parsable --array=1-$N_JOBS --gres=$GPU $SRCDIR/sh/slurm/test.s $PYTHONARGS)
