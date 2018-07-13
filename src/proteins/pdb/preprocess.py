f1f575b17c4e2c9a89af08547289718c438c795d
isaachenrion <isaachenrion@gmail.com>
2018-07-12 17:11:45 -0700

    preprocessing script

--- src/proteins/pdb/preprocess.py
+++ src/proteins/pdb/preprocess.py
@@ -2,51 +2,71 @@
 
 import time
 import os
+import logging
 import pickle
 
 from Bio.PDB import *
 from Bio.Alphabet import ThreeLetterProtein
 
-from .utils import *
+from utils import *
 
-def preprocess_pdb_directory(pdb_directory, file_format,save_path=None):
-    '''
-    Program for preprocessing a directory containing .pdb or .cif files.
-
-    Arguments
-        pdb_directory: the path of the directory to look inside
-        file_format: either 'pdb' or 'cif'
-        save_path: path to the file where the data will be saved
-
-    Output
-        x: list of numpy arrays which give one-hot encodings of the sequences.
-        y: list of distance matrices in Angstroms.
-    '''
+def preprocess_pdb_directory(pdb_directory, id_list_filename, save_dir, file_format, n=11):
+    '''Preprocess all of the pdb files in a directory that correspond to named ids in
+    a given id list. '''
     t = time.time()
+    parent_dir, child_dir = os.path.split(pdb_directory)
+    with open(id_list_filename, 'r') as f:
+        ids = f.read().split(',')
+
     if file_format == 'pdb':
         parser = PDBParser()
     elif file_format == 'cif':
         parser = MMCIFParser()
-    alphabet = [sym.upper() for sym in ThreeLetterProtein().letters]
-    x = []
-    y = []
-    for i, pdb_filename in enumerate(os.listdir(pdb_directory)):
-        id = pdb_filename.split('.')[0]
-        try:
-            structure = parser.get_structure(id, os.path.join(pdb_directory, pdb_filename))
-            seq, dists = get_distances(s)
-            seq_vec = np.array(_string_vectorizer(seq, alphabet))
-            assert np.all(seq_vec.sum(1) == 1)
-            x.append(seq_vec)
-            y.append(dists)
-        except UnicodeDecodeError:
-            print(pdb_filename)
-            raise ValueError("Found file with bad format! ({})".format(pdb_filename))
 
-    if save_path is None:
-        savedir = os.path.join('/'.join(pdb_directory.split('/')[:-1]), '..', 'preprocessed')
-        save_path = os.path.join(savedir, pdb_directory.split('/')[-1] + '.pkl')
-    print("Preprocessed {} proteins in {:.1f} seconds".format(len(x), time.time() - t))
-    with open(save_path, 'wb') as f:
-        pickle.dump(dict(x=x,y=y), f)
-    return x, y
+    alphabet = [sym.upper() for sym in ThreeLetterProtein().letters]
+    sequences = []
+    coords = []
+
+    for i, id in enumerate(ids):
+
+        pdb_id = id[:-1]
+        chain_id = id[-1]
+        pdb_filename = pdb_id + '.' + file_format
+
+        path_to_pdb = os.path.join(pdb_directory, pdb_filename)
+        structure = parser.get_structure(pdb_id, path_to_pdb)
+
+        seq_vec, coord = get_seq_and_coords(structure, chain_id, alphabet)
+
+        sequences.append(seq_vec)
+        coords.append(coord)
+
+        if i > 0 and i % 500 == 0:
+            logging.info("Preprocessed {} proteins".format(i))
+        if n is not None and i == n - 1:
+            break
+
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+    save_filename = os.path.join(save_dir, os.path.split(pdb_directory)[1] + '.pkl')
+    logging.info("Preprocessed {} proteins in {:.1f} seconds".format(len(sequences), time.time() - t))
+    with open(save_filename, 'wb') as f:
+        pickle.dump(dict(sequences=sequences,coords=coords), f)
+
+    return None
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description='Download PDB files')
+    parser.add_argument('-m', '--mode', type=str, default='test')
+    parser.add_argument('-i', '--id_dir', type=str, default='id_lists', help='Name of directory containing id list files')
+    parser.add_argument('-p', '--pdb_dir', type=str, default='pdbs', help='Name of directory containing pdb files')
+    parser.add_argument('-t', '--target_dir', type=str, default='preprocessed', help='Name of directory to put preprocessed file in')
+    parser.add_argument('-r', '--root_dir', type=str, default='/Users/isaachenrion/x/research/graphs/data/proteins/pdb25')
+    args = parser.parse_args()
+
+    id_list_filename = os.path.join(args.root_dir, args.id_dir, args.mode + '.txt')
+    pdb_directory = os.path.join(args.root_dir, args.pdb_dir, args.mode)
+    save_dir = os.path.join(args.root_dir, args.target_dir)
+
+    preprocess_pdb_directory(pdb_directory, id_list_filename, save_dir, 'pdb')
--- src/proteins/pdb/utils.py
+++ src/proteins/pdb/utils.py
@@ -2,26 +2,78 @@ import string
 import numpy as np
 
 def relevant_atom(residue):
-    if residue.get_resname() == 'GLY':
-        atom = 'CA'
+    '''Get the atom from a residue that is relevant for measuring residue-residue distances.
+    In all amino acids this is CB, except for glycine where it is CA. If neither of these
+    is present, return None'''
+    if residue.get_resname().upper() == 'GLY':
+        atom_id = 'CA'
     else:
-        atom = 'CB'
+        atom_id = 'CB'
+    try:
+        atom = residue[atom_id]
+    except KeyError:
+        atom = None
     return atom
 
-def get_distances(structure):
+def relevant_chain(structure, chain_id):
+    '''Given a structure and a chain id, return that chain from the structure.
+    This is robust to upper and lower case.'''
+    model = structure[0]
+    try:
+        chain = model[chain_id.upper()]
+    except KeyError:
+        try:
+            chain = model[chain_id.lower()]
+        except KeyError:
+            raise ValueError("Structure {} has no chain {}. It only has {}".format(structure.get_id(), chain_id, list(structure.get_chains())))
+    return chain
 
-    residues = list(structure.get_residues())
-    sequence = list(map(lambda r: r.get_resname(), residues))
-    dists = np.zeros((len(residues), len(residues)))
+def get_seq_and_coords(structure, chain_id, alphabet, distances=False):
+    '''Given a structure and chain id, return the sequence as a one-hot numpy
+    array, and the coordinates of the residues as a numpy array.
+    Optionally, return the matrix of distances between the residues.'''
+    chain = relevant_chain(structure, chain_id)
+
+    residues = list(chain.get_residues())
+    residues = [r for r in residues if r.get_resname() in alphabet]
+
+    # sequence
+    sequence = [r.get_resname() for r in residues]
+    seq_vec = _string_vectorizer(sequence, alphabet)
+    if not np.all(seq_vec.sum(1) == 1):
+        bad_indices = np.where(seq_vec.sum(1) != 1)[0]
+        bad_residues = [r.get_resname() for i, r in enumerate(residues) if i in bad_indices]
+        out_str = "Sequence {}/{} has not been correctly one-hot encoded.\n".format(pdb_id, chain_id)
+        out_str += "Indices {} in the sequence were incorrectly encoded\n".format(', '.join(bad_indices))
+        our_str += "These correspond to the following residues: {}".format(', '.join(bad_residues))
+        raise ValueError(out_str)
+
+    # coords
+    coords = np.zeros((len(residues), 3))
     for i, r1 in enumerate(residues):
         a1 = relevant_atom(r1)
-        for j, r2 in enumerate(residues):
-            a2 = relevant_atom(r2)
-            dists[i, j] = r1[a1] - r2[a2]
+        if a1 is not None:
+            xyz = a1.get_coord()
+            coords[i] = xyz
+        else:
+            coords[i] = [np.inf, np.inf, np.inf]
 
-    return sequence, dists
+    # distances
+    if distances:
+        dists = np.zeros((len(residues), len(residues)))
+        for i, r1 in enumerate(residues):
+            a1 = relevant_atom(r1)
+            for j, r2 in enumerate(residues):
+                a2 = relevant_atom(r2)
+                if a1 is not None and a2 is not None:
+                    dists[i, j] = a1 - a2
+                else:
+                    dists[i, j] = np.inf
+        return seq_vec, coords, dists
+    return seq_vec, coords
 
 def _string_vectorizer(strng, alphabet=string.ascii_uppercase):
-    vector = [[0 if char != letter else 1 for char in alphabet]
-                  for letter in strng]
+    '''Given a string and alphabet, return a one-hot representation as a numpy array'''
+    vector = np.array([[0 if char != letter else 1 for char in alphabet]
+                  for letter in strng])
     return vector
